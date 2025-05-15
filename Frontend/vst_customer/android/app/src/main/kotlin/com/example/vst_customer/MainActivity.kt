34140a862353ae1dd054775ad63ba739ad4eb033
Django Backend <django_user@ubuntu-cloudpanel.localhost>
2025-05-15 01:42:33 +0000

    From Server

--- Frontend/vst_customer/android/app/src/main/kotlin/com/example/vst_customer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vst_customer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Frontend/vst_staffs/android/app/src/main/kotlin/com/example/vst_staffs/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vst_staffs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
